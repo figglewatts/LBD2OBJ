b1f0240ad52011f8169875b6c8e2b1f909d5db05
Figglewatts <figglewatts@hotmail.co.uk>
2015-12-20 23:31:16 +0000

    Implement TMD parsing fully

--- LBD2OBJ/Program.cs
+++ LBD2OBJ/Program.cs
@@ -80,6 +80,7 @@ namespace LBD2OBJ
 			obj.normals = new NORMAL[obj.numNorms];
 			obj.primTop = b.ReadUInt32();
 			obj.numPrims = b.ReadUInt32();
+			obj.primitives = new PRIMITIVE[obj.numPrims];
 			obj.scale = b.ReadInt32();
 
 			long cachedPosition = b.BaseStream.Position; // cache position so we can return
@@ -97,6 +98,14 @@ namespace LBD2OBJ
 			{
 				obj.normals[n] = readNormal(b);
 			}
+
+			// go to address of primitives for object and load them
+			b.BaseStream.Seek(objTop + obj.primTop, SeekOrigin.Begin);
+			for (int p = 0; p < obj.numPrims; p++)
+			{
+				obj.primitives[p] = readPrimitive(b);
+			}
+
 			b.BaseStream.Seek(cachedPosition, SeekOrigin.Begin); // return to cached position to load next object
 
 			return obj;
@@ -129,30 +138,404 @@ namespace LBD2OBJ
 			p.ilen = b.ReadByte();
 			p.flag = b.ReadByte();
 			p.mode = b.ReadByte();
-			p.classification = readPrimitiveClassification(b, p.mode);
+			p.classification = readPrimitiveClassification(b, p.mode, p.flag);
 			p.data = readPrimitiveData(b, p.classification);
 			return p;
 		}
 
-		private static PRIMITIVECLASSIFICATION readPrimitiveClassification(BinaryReader b, int mode)
+		private static PRIMITIVECLASSIFICATION readPrimitiveClassification(BinaryReader b, int mode, int flags)
 		{
 			PRIMITIVECLASSIFICATION classification;
-			// it's a bitmask
+			// a bitmask of 11100000 to check if the mode is 001, i.e. a polygon
 			if ((mode & 224) != 32) { throw new InvalidDataException("Unrecognized polygon mode"); }
 			classification.gouraudShaded = intToBool(mode & 1);
 			mode >>= 1;
 			classification.quad = intToBool(mode & 1);
 			mode >>= 1;
 			classification.textureMapped = intToBool(mode & 1);
-			mode >>= 2; // skip bit at 6, we don't need it
-			classification.unlit = intToBool(mode & 1);
+
+			classification.unlit = intToBool(flags & 1);
+			flags >>= 1;
+			classification.twoSided = intToBool(flags & 1);
+			flags >>= 1;
+			classification.gradation = intToBool(flags & 1);
+			
 			return classification;
 
 		}
 
+		// this method, lol, hope you like nested if statements
 		private static PRIMITIVEDATA readPrimitiveData(BinaryReader b, PRIMITIVECLASSIFICATION classification)
 		{
-			
+			PRIMITIVEDATA data = new PRIMITIVEDATA();
+			if (classification.quad)
+			{
+				if (classification.unlit)
+				{
+					if (classification.gradation)
+					{
+						if (classification.textureMapped)
+						{
+							// fig. 2-54 d
+							data.uvCoords = new UV[4];
+							for (int i = 0; i < 4; i++)
+							{
+								data.uvCoords[i] = readUV(b);
+								skipBytes(2, b);
+							}
+							skipBytes(16, b);
+							data.triangleIndices = new short[4];
+							for (int i = 0; i < 4; i++)
+							{
+								data.triangleIndices[i] = b.ReadInt16();
+							}
+
+						}
+						else
+						{
+							// fig. 2-54 c
+							skipBytes(16, b);
+							data.triangleIndices = new short[4];
+							for (int i = 0; i < 4; i++)
+							{
+								data.triangleIndices[i] = b.ReadInt16();
+							}
+						}
+					}
+					else
+					{
+						if (classification.textureMapped)
+						{
+							// fig. 2-54 b
+							data.uvCoords = new UV[4];
+							for (int i = 0; i < 4; i++)
+							{
+								data.uvCoords[i] = readUV(b);
+								skipBytes(2, b);
+							}
+							skipBytes(4, b);
+							data.triangleIndices = new short[4];
+							for (int i = 0; i < 4; i++)
+							{
+								data.triangleIndices[i] = b.ReadInt16();
+							}
+						}
+						else
+						{
+							// fig. 2-54 a
+							skipBytes(4, b);
+							data.triangleIndices = new short[4];
+							for (int i = 0; i < 4; i++)
+							{
+								data.triangleIndices[i] = b.ReadInt16();
+							}
+						}
+					}
+				}
+				else
+				{
+					if (classification.gouraudShaded)
+					{
+						if (classification.textureMapped)
+						{
+							// fig. 2-50 f
+							data.uvCoords = new UV[4];
+							for (int i = 0; i < 4; i++)
+							{
+								data.uvCoords[i] = readUV(b);
+								skipBytes(2, b);
+							}
+
+							data.normalIndices = new short[4];
+							data.triangleIndices = new short[4];
+							for (int i = 0; i < 4; i++)
+							{
+								data.normalIndices[i] = b.ReadInt16();
+								data.triangleIndices[i] = b.ReadInt16();
+							}
+						}
+						else
+						{
+							if (classification.gradation)
+							{
+								// fig. 2-50 e
+								skipBytes(16, b);
+
+								data.normalIndices = new short[4];
+								data.triangleIndices = new short[4];
+								for (int i = 0; i < 4; i++)
+								{
+									data.normalIndices[i] = b.ReadInt16();
+									data.triangleIndices[i] = b.ReadInt16();
+								}
+							}
+							else
+							{
+								// fig. 2-50 d
+								skipBytes(4, b);
+
+								data.normalIndices = new short[4];
+								data.triangleIndices = new short[4];
+								for (int i = 0; i < 4; i++)
+								{
+									data.normalIndices[i] = b.ReadInt16();
+									data.triangleIndices[i] = b.ReadInt16();
+								}
+							}
+						}
+					}
+					else
+					{
+						if (classification.textureMapped)
+						{
+							// fig. 2-50 c
+							data.uvCoords = new UV[4];
+							for (int i = 0; i < 4; i++)
+							{
+								data.uvCoords[i] = readUV(b);
+								skipBytes(2, b);
+							}
+
+							data.normalIndices = new short[1];
+							data.normalIndices[0] = b.ReadInt16();
+
+							data.triangleIndices = new short[4];
+							for (int i = 0; i < 4; i++)
+							{
+								data.triangleIndices[i] = b.ReadInt16();
+							}
+							skipBytes(2, b);
+						}
+						else
+						{
+							if (classification.gradation)
+							{
+								// fig. 2-50 b
+								skipBytes(16, b);
+
+								data.normalIndices = new short[1];
+								data.normalIndices[0] = b.ReadInt16();
+
+								data.triangleIndices = new short[4];
+								for (int i = 0; i < 4; i++)
+								{
+									data.triangleIndices[i] = b.ReadInt16();
+								}
+								skipBytes(2, b);
+							}
+							else
+							{
+								// fig. 2-50 a
+								skipBytes(4, b);
+
+								data.normalIndices = new short[1];
+								data.normalIndices[0] = b.ReadInt16();
+
+								data.triangleIndices = new short[4];
+								for (int i = 0; i < 4; i++)
+								{
+									data.triangleIndices[i] = b.ReadInt16();
+								}
+								skipBytes(2, b);
+							}
+						}
+					}
+				}
+			}
+			else
+			{
+				if (classification.unlit)
+				{
+					if (classification.gradation)
+					{
+						if (classification.textureMapped)
+						{
+							// fig. 2-52 d
+							data.uvCoords = new UV[3];
+							for (int i = 0; i < 3; i++)
+							{
+								data.uvCoords[i] = readUV(b);
+								skipBytes(2, b);
+							}
+							skipBytes(12, b);
+
+							data.triangleIndices = new short[3];
+							for (int i = 0; i < 3; i++)
+							{
+								data.triangleIndices[i] = b.ReadInt16();
+							}
+							skipBytes(2, b);
+						}
+						else
+						{
+							// fig. 2-52 c
+							skipBytes(12, b);
+
+							data.triangleIndices = new short[3];
+							for (int i = 0; i < 3; i++)
+							{
+								data.triangleIndices[i] = b.ReadInt16();
+							}
+							skipBytes(2, b);
+						}
+					}
+					else
+					{
+						if (classification.textureMapped)
+						{
+							// fig. 2-52 b
+							data.uvCoords = new UV[3];
+							for (int i = 0; i < 3; i++)
+							{
+								data.uvCoords[i] = readUV(b);
+								skipBytes(2, b);
+							}
+							skipBytes(4, b);
+
+							data.triangleIndices = new short[3];
+							for (int i = 0; i < 3; i++)
+							{
+								data.triangleIndices[i] = b.ReadInt16();
+							}
+							skipBytes(2, b);
+						}
+						else
+						{
+							// fig. 2-52 a
+							skipBytes(4, b);
+
+							data.triangleIndices = new short[3];
+							for (int i = 0; i < 3; i++)
+							{
+								data.triangleIndices[i] = b.ReadInt16();
+							}
+							skipBytes(2, b);
+						}
+					}
+				}
+				else
+				{
+					if (classification.gouraudShaded)
+					{
+						if (classification.textureMapped)
+						{
+							// fig. 2-48 f
+							data.uvCoords = new UV[3];
+							for (int i = 0; i < 3; i++)
+							{
+								data.uvCoords[i] = readUV(b);
+								skipBytes(2, b);
+							}
+
+							data.normalIndices = new short[3];
+							data.triangleIndices = new short[3];
+							for (int i = 0; i < 3; i++)
+							{
+								data.normalIndices[i] = b.ReadInt16();
+								data.triangleIndices[i] = b.ReadInt16();
+							}
+						}
+						else
+						{
+							if (classification.gradation)
+							{
+								// fig. 2-48 e
+								skipBytes(12, b);
+
+								data.normalIndices = new short[3];
+								data.triangleIndices = new short[3];
+								for (int i = 0; i < 3; i++)
+								{
+									data.normalIndices[i] = b.ReadInt16();
+									data.triangleIndices[i] = b.ReadInt16();
+								}
+							}
+							else
+							{
+								// fig. 2-48 d
+								skipBytes(4, b);
+
+								data.normalIndices = new short[3];
+								data.triangleIndices = new short[3];
+								for (int i = 0; i < 3; i++)
+								{
+									data.normalIndices[i] = b.ReadInt16();
+									data.triangleIndices[i] = b.ReadInt16();
+								}
+							}
+						}
+					}
+					else
+					{
+						if (classification.textureMapped)
+						{
+							// fig. 2-48 c
+							data.uvCoords = new UV[3];
+							for (int i = 0; i < 3; i++)
+							{
+								data.uvCoords[i] = readUV(b);
+								skipBytes(2, b);
+							}
+
+							data.normalIndices = new short[1];
+							data.normalIndices[0] = b.ReadInt16();
+
+							data.triangleIndices = new short[3];
+							for (int i = 0; i < 3; i++)
+							{
+								data.triangleIndices[i] = b.ReadInt16();
+							}
+						}
+						else
+						{
+							if (classification.gradation)
+							{
+								// fig. 2-48 b
+								skipBytes(12, b);
+
+								data.normalIndices = new short[1];
+								data.normalIndices[0] = b.ReadInt16();
+
+								data.triangleIndices = new short[3];
+								for (int i = 0; i < 3; i++)
+								{
+									data.triangleIndices[i] = b.ReadInt16();
+								}
+							}
+							else
+							{
+								// fig. 2-48 a
+								skipBytes(4, b);
+
+								data.normalIndices = new short[1];
+								data.normalIndices[0] = b.ReadInt16();
+
+								data.triangleIndices = new short[3];
+								for (int i = 0; i < 3; i++)
+								{
+									data.triangleIndices[i] = b.ReadInt16();
+								}
+								
+							}
+                        }
+					}
+				}
+			}
+			return data;
+		}
+
+		private static UV readUV(BinaryReader b)
+		{
+			UV uv;
+			uv.U = b.ReadByte();
+			uv.V = b.ReadByte();
+			return uv;
+
+		}
+
+		private static void skipBytes(long bytes, BinaryReader b)
+		{
+			b.BaseStream.Seek(bytes, SeekOrigin.Current);
 		}
 
 		private static bool intToBool(int input)
