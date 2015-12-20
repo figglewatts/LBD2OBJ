7d9c0be6f3b5d879773d56a22a0e7b16c9bdff56
Figglewatts <figglewatts@hotmail.co.uk>
2015-12-20 15:23:12 +0000

    Add structs for TMD file

---
+++ LBD2OBJ/Types/TMD.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LBD2OBJ.Types
+{
+	struct TMDHEADER
+	{
+		uint ID; // should be 0x41
+		uint flags; // 0 if addresses are relative from top of block, 1 if from start of file
+		uint numObjects;
+	}
+
+	struct OBJECT
+	{
+		uint vertTop; // pointer to start of vertices
+		uint numVerts;
+		uint normTop; // pointer to start of normals
+		uint numNorms;
+		uint primTop; // pointer to start of primitives
+		uint numPrims;
+		int scale; // unused
+	}
+
+	struct PRIMITIVE
+	{
+		byte mode; // b001 - poly, b010 - line, b011 - sprite
+		byte flag;
+		byte ilen;
+		byte olen;
+	}
+
+	struct VERTEX
+	{
+		short X;
+		short Y;
+		short Z;
+	}
+
+	struct TMD
+	{
+		TMDHEADER header;
+		OBJECT[] objTable;
+	}
+}
