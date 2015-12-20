99e9b86fe855b52acd68db5d5ca1c8bddc9d6025
Figglewatts <figglewatts@hotmail.co.uk>
2015-12-20 15:23:25 +0000

    Add class for fixed point data

---
+++ LBD2OBJ/Types/FixedPoint.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LBD2OBJ.Types
+{
+	class FixedPoint
+	{
+		public int IntegralPart { get; set; }
+		public int DecimalPart { get; set; }
+
+		public FixedPoint(byte[] data)
+		{
+			if (data.Length != 2) { throw new ArgumentException("data must be 2 bytes", "data"); }
+
+			byte[] _data = new byte[2];
+			data.CopyTo(_data, 0);
+
+			var signMask = (byte)128;
+			var integralMask = (byte)112;
+			var firstPartOfDecimalMask = (byte)15;
+
+			bool isNegative = (_data[0] & signMask) == 128;
+			int integralPart = (_data[0] & integralMask) * (isNegative ? -1 : 1);
+			int decimalPart = (_data[0] & firstPartOfDecimalMask);
+			decimalPart <<= 8;
+			decimalPart += data[1];
+
+			IntegralPart = integralPart;
+			DecimalPart = decimalPart;
+		}
+	}
+}
