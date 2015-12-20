bc5ad326f728d5da6ed92091bb03e9b2f7d9555b
Figglewatts <figglewatts@hotmail.co.uk>
2015-12-20 18:40:45 +0000

    Change data types in TMD structs to be correct

--- LBD2OBJ/Types/TMD.cs
+++ LBD2OBJ/Types/TMD.cs
@@ -8,40 +8,50 @@ namespace LBD2OBJ.Types
 {
 	struct TMDHEADER
 	{
-		uint ID; // should be 0x41
-		uint flags; // 0 if addresses are relative from top of block, 1 if from start of file
-		uint numObjects;
+		public uint ID; // should be 0x41
+		public uint flags; // 0 if addresses are relative from top of block, 1 if from start of file
+		public uint numObjects;
 	}
 
 	struct OBJECT
 	{
-		uint vertTop; // pointer to start of vertices
-		uint numVerts;
-		uint normTop; // pointer to start of normals
-		uint numNorms;
-		uint primTop; // pointer to start of primitives
-		uint numPrims;
-		int scale; // unused
+		public uint vertTop; // pointer to start of vertices
+		public uint numVerts;
+		public uint normTop; // pointer to start of normals
+		public uint numNorms;
+		public uint primTop; // pointer to start of primitives
+		public uint numPrims;
+		public int scale; // unused
 	}
 
 	struct PRIMITIVE
 	{
-		byte mode; // b001 - poly, b010 - line, b011 - sprite
-		byte flag;
-		byte ilen;
-		byte olen;
+		public byte mode; // b001 - poly, b010 - line, b011 - sprite
+		public byte flag; // rendering info
+		public byte ilen; // length (in words) of packet data
+		public byte olen; // length (in words) of 2D drawing primitives
 	}
 
 	struct VERTEX
 	{
-		short X;
-		short Y;
-		short Z;
+		public short X;
+		public short Y;
+		public short Z;
+	}
+
+	struct NORMAL
+	{
+		public FixedPoint nX;
+		public FixedPoint nY;
+		public FixedPoint nZ;
 	}
 
 	struct TMD
 	{
-		TMDHEADER header;
-		OBJECT[] objTable;
+		public TMDHEADER header;
+		public OBJECT[] objTable;
+		public PRIMITIVE[] primTable;
+		public VERTEX[] vertTable;
+		public NORMAL[] normTable;
 	}
 }
