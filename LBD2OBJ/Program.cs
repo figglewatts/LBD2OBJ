ada8577d805b23bd226b3f4cfccb27dac5c0f35a
Figglewatts <figglewatts@hotmail.co.uk>
2015-12-20 14:27:06 +0000

    Create Visual Studio project

---
+++ LBD2OBJ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LBD2OBJ
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
