286e5428ca046470a02069ee8b7dccea67154292
Figglewatts <figglewatts@hotmail.co.uk>
2016-06-13 18:39:54 +0100

    Created library for LBD conversion

---
+++ LBD2OBJLib/LBDConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LBD2OBJLib
+{
+    public static class LBDConverter
+    {
+    }
+}
