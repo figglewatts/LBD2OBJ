ea2b79f3c1a74c7809603730c2e21031dae0d67c
Figglewatts <figglewatts@hotmail.co.uk>
2016-06-13 20:36:08 +0100

    Change mask variables to const values, remove from constructor

--- LBD2OBJLib/Types/FixedPoint.cs
+++ LBD2OBJLib/Types/FixedPoint.cs
@@ -12,6 +12,10 @@ namespace LBD2OBJLib.Types
 		public int IntegralPart { get; set; }
 		public int DecimalPart { get; set; }
 
+		const byte SIGN_MASK = 128;
+		const byte INTEGRAL_MASK = 112;
+		const byte MANTISSA_MASK = 15;
+
 		public FixedPoint(byte[] data)
 		{
 			if (data.Length != 2) { throw new ArgumentException("data must be 2 bytes", "data"); }
@@ -19,13 +23,11 @@ namespace LBD2OBJLib.Types
 			byte[] _data = new byte[2];
 			data.CopyTo(_data, 0);
 
-			var signMask = (byte)128;
-			var integralMask = (byte)112;
-			var firstPartOfDecimalMask = (byte)15;
+			
 
-			bool isNegative = (_data[0] & signMask) == 128;
-			int integralPart = (_data[0] & integralMask) * (isNegative ? -1 : 1);
-			int decimalPart = (_data[0] & firstPartOfDecimalMask);
+			bool isNegative = (_data[0] & SIGN_MASK) == 128;
+			int integralPart = (_data[0] & INTEGRAL_MASK) * (isNegative ? -1 : 1);
+			int decimalPart = (_data[0] & MANTISSA_MASK);
 			decimalPart <<= 8;
 			decimalPart += data[1];
 
